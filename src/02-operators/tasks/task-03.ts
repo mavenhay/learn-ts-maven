b114d700a3061b349fc9c2172cb0b6b8faad384c
mavenhay <maven.hay@gmail.com>
2026-07-29 15:05:56 +0700

    task

--- src/02-operators/tasks/task-03.ts
+++ src/02-operators/tasks/task-03.ts
@@ -12,8 +12,22 @@
  * The company has the following policy:
  * Employees who work more than 10 overtime hours receive an additional Rp300,000 performance bonus.
  * Otherwise, no bonus is given.
- * You need to calculatea and display:
+ * You need to calculate and display:
  * - Overtime pay
  * - Bonus
  * - Final salary
  */
+
+let basicSalary:number=5000000,overtimeHours:number=12,overtimeRate:number=500000,overtimePay:number=0, bonus:number=0, finalSalary:number=0;
+
+overtimePay = overtimeRate*overtimeHours;
+bonus = overtimeHours > 10 ? 300000:0;
+finalSalary = overtimePay+overtimePay+bonus
+
+console.log(`
+===== Employee Information =====
+Employee Name: Dimas
+Overtime Pay: ${overtimePay}
+Bonus: ${bonus}
+Final Salary; ${finalSalary}
+`)
