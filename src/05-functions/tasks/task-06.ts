aa682b6d149014ad2c9a82f6d1d24ed7545a8095
mavenhay <maven.hay@gmail.com>
2026-08-12 12:48:12 +0700

    Task

--- src/05-functions/tasks/task-06.ts
+++ src/05-functions/tasks/task-06.ts
@@ -12,35 +12,26 @@
  * Create the following functions:
  */
 
-const sales = [
-  125000,
-  780000,
-  250000,
-  99000,
-  540000,
-  670000,
-  180000,
-  450000,
-  310000,
-  820000
-];
+const sales = [125000,780000,250000,99000,540000,670000,180000,450000,310000,820000];
+
 
 function calculateTotalSales(sales: number[]): number {
-
+  return sales.reduce((t,n)=>t+n,0);;
 }
 
 function findHighestTransaction(sales: number[]): number {
-
+  return sales.sort()[sales.length-1];
 }
 
 function findLowestTransaction(sales: number[]): number {
-
+  return sales.sort()[0];
 }
 
 function calculateAverageSale(sales: number[]): number {
-
+  return calculateTotalSales(sales)/sales.length;
 }
 
 function countLargeTransactions(sales: number[], minimumAmount: number): number {
-
+  return sales.reduce((t,n,i)=>(sales[i]>=minimumAmount)?t+1:t,0);
 }
+
--- src/05-functions/tasks/task-07.ts
+++ src/05-functions/tasks/task-07.ts
@@ -11,7 +11,9 @@
  * Complete following functions!
  */
 
-const students = [
+interface Student {name:string,major:string,active:boolean};
+
+const students:Student[] = [
   {
     name: "Alya",
     major: "Software Engineering",
@@ -39,18 +41,47 @@ const students = [
   }
 ];
 
-// function countActiveStudents(...){
+function countActiveStudents(students:Student[]):number{
+  return students.reduce((t,n,i)=>students[i].active?t+1:t,0);
+}
 
-// }
+function countInactiveStudents(students:Student[]):number{
+  return students.reduce((t,n,i)=>students[i].active?t:t+i,0);
+}
 
-// function countInactiveStudents(...){
+function enroll(students:Student[]):[string,Student[]][] {
+  let a:string[] = [], b:[string,Student[]][] = [];
+  students.forEach(s=>{
+    if (!a.includes(s.major)){
+      a[a.length] = s.major;
+      b[b.length] = [s.major,[]];
+    }
+    // console.log(a,b)
+    let temp = b[a.indexOf(s.major)][1];
+    temp[temp.length] = s;
+  });
+  return b;
+}
 
-// }
+function countStudentsByMajor(students:[string,Student[]][]):[string,number][]{
+  let b:[string,number][]=[];
+  students.forEach(s => {
+    b[b.length][0] = s[0];
+    b[b.length][1] = s[1].reduce((t)=>t+1,0);
+  });
+  return b;
+}
 
-// function countStudentsByMajor(...){
+function printEnrollmentReport(enrollment:[string,Student[]][]):void{
+  enrollment.forEach(e=>{
+    console.log(`${e[0]} Major:`);
+    e[1].forEach(i=>{
+      console.log(`- ${i.name}`);
+    });
+    console.log();
+  });
+}
 
-// }
+let asd = enroll(students);
 
-// function printEnrollmentReport(...){
-    
-// }
+printEnrollmentReport(asd);
